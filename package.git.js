029651c3165705dead0f5f3d43086a475ace1f8c
or01558 <orchoo9@gmail.com>
2021-03-04 15:22:49 +0200

    a

---
+++ package.git.js
@@ -0,0 +1,43 @@
+module.exports.git = JSON.parse({
+    "name": "@or01558/vars-addons",
+    "version": "1.0.8",
+    "description": "",
+    "main": "index.js",
+    "scripts": {
+        "debug": "nodemon test.js"
+    },
+    "author": "",
+    "license": "ISC",
+    "homepage": "https://github.com/or01558/Vars-Addons",
+    "repository": {
+        "url": "git@github.com:or01558/vars-Addons.git"
+    },
+    "publishConfig": {
+        "registry": "https://npm.pkg.github.com/or01558" 
+    },
+    "devDependencies": {
+        "nodemon": "^2.0.7"
+    }
+});
+
+module.exports.npm = JSON.parse({
+    "name": "@or01558/vars-addons",
+    "version": "1.0.8",
+    "description": "",
+    "main": "index.js",
+    "scripts": {
+        "debug": "nodemon test.js"
+    },
+    "author": "",
+    "license": "ISC",
+    "homepage": "https://github.com/or01558/Vars-Addons",
+    "repository": {
+        "url": "git@github.com:or01558/vars-Addons.git"
+    },
+    "publishConfig": {
+        "registry": "https://registry.npmjs.org" 
+    },
+    "devDependencies": {
+        "nodemon": "^2.0.7"
+    }
+});
